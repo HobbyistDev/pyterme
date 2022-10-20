090b04053bb71bf76d0f9a4a15a2434e93f55f61
HobbyistDev <tesutonihon4@gmail.com>
2022-10-20 18:49:44 +0900

    [core:shell_conf_parser] set default return to dictionary

--- core/shell_conf_parser.py
+++ core/shell_conf_parser.py
@@ -5,4 +5,4 @@ def shell_set_configuration(conf_path='default'):
     if conf_path == 'default':
         with open(pathlib.Path("conf/shell_conf.json")) as file:
             json_conf_file = json.load(file)
-        return json_conf_file.get('shell')
+        return json_conf_file.get('shell') or {}
