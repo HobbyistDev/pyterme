2769dc86eec2c9132005207b277925f25ce31506
HobbyistDev <tesutonihon4@gmail.com>
2022-10-20 09:12:50 +0900

    [core:shell_argument] refactor argument parser, add --set-prompt-symbol argument

--- core/shell_argument.py
+++ core/shell_argument.py
@@ -1,13 +1,24 @@
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
+parser = argparse.ArgumentParser(
+    prog='pyterme', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+# shell customization
+shell_customization_parser = parser.add_argument_group('Shell Customization Option')
+shell_customization_parser.add_argument(
     '--set-env', default='default', metavar='ENV',
     help=(
         '''set environment type in shell. This argument accept three option: \n
-        Windows, linux, and default (default=\'default\')'''))
-parser.add_argument(
+        Windows, linux, and default'''))
+
+shell_customization_parser.add_argument(
     '--set-prompt-text-style', default='unix', metavar='prompt_style',
     help='''This command used to change the prompt to be looks like POSIX or Windows Prompt'''
 )
+
+shell_customization_parser.add_argument(
+    '--set-prompt-symbol', default='$', metavar='symbol',
+    help='Set particular prompt symbol to terminal. This option didn\'t take effect when --set-prompt-text-style set to windows'
+)
+
 args = parser.parse_args()
