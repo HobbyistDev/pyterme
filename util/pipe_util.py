bc4e36f71b7c686cc317f0f51199c9a2c319f102
HobbyistDev <tesutonihon4@gmail.com>
2022-10-21 08:03:15 +0900

    [util:pipe_util] close stderr/stdout target in the end of `to_stderr` or `to_stdout`

--- util/pipe_util.py
+++ util/pipe_util.py
@@ -3,7 +3,9 @@ import sys
 def to_stdout(input_, end='\n', stdout_target=sys.stdout):
     if input:
         print(str(input_), end=end, file=stdout_target, flush=True)
-    
+    stdout_target.close()
+
 def to_stderr(input_, end='\n',stderr_target=sys.stderr):
     if input:
         print(str(input_), end=end, file=stderr_target)
+    stderr_target.close()
