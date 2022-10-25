fb49b907153b6543a9fd062d8f4196000f74547d
HobbyistDev <tesutonihon4@gmail.com>
2022-10-26 08:32:31 +0900

    [test:which] Add test case

--- command/which/which.py
+++ command/which/which.py
@@ -8,5 +8,4 @@ class Which(CommandSet):
 
     def command(self, file_path=None, *args, **kwargs) -> str:
         if file_path is not None:
-            if shutil.which(file_path) is not None:
-                return shutil.which(file_path)
+            return shutil.which(file_path)
--- test/test_which.py
+++ test/test_which.py
@@ -1,13 +1,19 @@
 import unittest
-# from command.which.which import Which
+
+from command.which.which import Which
 
 
 class TestWhich(unittest.TestCase):
+    def setUp(self) -> None:
+        self.which_command = Which()
+
     def test_which_without_argument(self):
-        pass
+        command_result = self.which_command.run_command()
+        self.assertEqual((None, None), command_result)
 
     def test_which_with_valid_argument(self):
         pass
 
     def test_which_with_invalid_argument(self):
-        pass
+        command_result = self.which_command.run_command('dfghj')
+        self.assertEqual((None, None), command_result)
