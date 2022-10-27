b7155465ff7d279496341226d9e455b5fdff2ae2
HobbyistDev <tesutonihon4@gmail.com>
2022-10-27 09:04:27 +0900

    [test:echo] Add `test_echo_with_one_user_argument` test case

--- test/test_echo.py
+++ test/test_echo.py
@@ -9,3 +9,7 @@ class TestEcho(unittest.TestCase):
     def test_echo_without_argument(self):
         result = self.echo_command.run_command()
         self.assertEqual('\n', result[0])
+
+    def test_echo_with_one_user_argument(self):
+        result = self.echo_command.run_command('hello')
+        self.assertEqual(('hello', None), result)
