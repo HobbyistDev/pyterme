126def1cf6a29df3cf998b85183adf484cb377b2
HobbyistDev <tesutonihon4@gmail.com>
2022-11-14 08:32:15 +0900

    [lexer] Set RegexLexer and ShlexLexer in builtin lexer

--- builtin/lexer/shell_lexer.py
+++ builtin/lexer/shell_lexer.py
@@ -1,4 +1,5 @@
 import re
+import shlex
 
 from .token import (
     Float,
@@ -9,10 +10,63 @@ from .token import (
 )
 
 
-class ShellLexer:
+class ShellLexerBase:
     def __init__(self, text):
         self.text: str = text
+        self.raw_token = []
+        self.token_list = []
 
+    def lex(self) -> list:
+        self.set_token_value_list()
+        self.tokenizer()
+        return self.token_list
+
+    def set_token_value_list(self):
+        pass
+
+    def tokenizer(self):
+        self.token_list = []
+        for index, token in enumerate(self.raw_token):
+            token_type = 'undefined'
+
+            # aritmethic
+            if token == '+':
+                token_type = 'Aritmethic'
+
+            elif token == '-':
+                token_type = 'Aritmethic'
+
+            elif token == '/':
+                token_type = 'Arithmetic'
+
+            # Pipe symbol
+            elif token == '>':
+                token_type = PipeToken
+
+            elif token == '>>':
+                token_type = PipeToken
+
+            elif token == '<':
+                token_type = PipeToken
+
+            elif token == '|':
+                token_type = PipeToken
+
+            # data type
+            elif re.match(r'\w+', token):
+                token_type = String
+
+            elif re.match(r'\d', token):
+                token_type = Integer
+
+            elif re.match(r'\d+\.\d+', token):
+                token_type = Float
+
+            if token_type != 'undefined':
+                self.token_list.append(Token(token, token_type, index))
+
+
+class RegexLexer(ShellLexerBase):
     def _set_to_tokens(self) -> list[str]:
         pipe_pattern = r'2>|1>|>>|\|'
         number_pattern = r'd+'
@@ -69,3 +123,8 @@ class ShellLexer:
 
     def lex(self, log_level='warning'):
         pass
+
+
+class ShlexLexer(ShellLexerBase):
+    def set_token_value_list(self) -> list:
+        self.raw_token = list(shlex.shlex(self.text, punctuation_chars=True))
